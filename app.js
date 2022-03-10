1e4caa72d1cecdd3756584d2008196110530a19e
zuriBarnes <45982373+zuriBarnes@users.noreply.github.com>
2022-03-10 18:20:46 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+// express app
+const app = express();
+
+app.set("view engine", "ejs");
+
+// listen for requests
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  const blogs = [
+    // { title: "Mario", snippet: "ffhjfdp[j[pkryjk" },
+    // { title: "Ryu", snippet: "gkegpsts" },
+    // { title: "Star", snippet: "poergjfnfkwkg" },
+  ];
+  res.render("index", { title: "Home", blogs });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
+});
+
+//   404 Page
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
+
+// app.get("", (req, res) => {
+//   res.sendFile("./views/404.html", { root: __dirname });
+// });
